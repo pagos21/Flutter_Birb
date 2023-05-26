1be355b2d49a3bc550e825888c3549dd75c9e756
Nicolas Paganelli <npaganelli@proce.net>
2023-05-26 10:52:31 +0200

    aggiunto controllo GameOver + script di comunicazione con Flutter

--- Assets/LogicScript.cs
+++ Assets/LogicScript.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using FlutterUnityIntegration;
 
 public class LogicScript : MonoBehaviour
 {
     public int playerScore;
     public Text scoreTxt;
     public GameObject gameOverScreen;
+    public UnityMessageManager mess;
+    public bool isGameOver = false;
 
     [ContextMenu("Increase score")]
     public void addScore(int score2Add)
@@ -19,11 +22,14 @@ public class LogicScript : MonoBehaviour
 
     public void resetGame()
     {
+        isGameOver = false;
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
     public void gameOver()
     {
+        mess.SendMessageToFlutter(scoreTxt.text);
+        isGameOver = true;
         gameOverScreen.SetActive(true);
     }
 }
